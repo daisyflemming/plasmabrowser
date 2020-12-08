50888a0161c81de3b9da40dcb7756baee92707ba
daisyflemming <daisyflemming@gmail.com>
2020-12-07 22:29:32 -0500

    check in all I have for now

--- ui/src/components/plots/BarPlot.js
+++ ui/src/components/plots/BarPlot.js
@@ -1,43 +1,31 @@
 import React, {useEffect, useRef} from 'react';
 import {connect} from 'react-redux';
-import {create_UUID} from "../utils/commons";
 import * as d3 from 'd3';
-
-const data = [12, 5, 6, 6, 9, 10];
-const prepareDataPoints = (expressionCounts) =>{
-  expressionCounts.map(s => {
-    return (
-      <div key={'count_' + create_UUID()}>
-        &emsp;{"{"}<br/>
-        &ensp;&emsp;"count": {s.count},<br/>
-        &ensp;&emsp;"name": "{s.name}",<br/>
-        &ensp;&emsp;"range": {s.range.map(x =>
-        <span key={'range_' + create_UUID()}>
-                  {"{"} "start": {x.start}, "end": {x.end} {"}"}
-                </span>
-      )}<br/>
-        &emsp;{"}"},<br/>
-      </div>
-    )
-  })
-}
 
 const createBarChart =(svg, data) =>{
   let margin = 100,
     width = svg.attr("width") - margin,
     height = svg.attr("height") - margin;
+
+  const xScaleValue = data.reduce((acc, curr) => {
+    return acc + curr.x
+  }, 0)
+
   let xScale = d3.scaleLinear()
-    .domain([d3.min(data, function(d) { return d.range[0].start; }), d3.max(data, function(d) { return d.range[0].start; })])
+    .domain([
+      d3.min(data, function(d) { return d.x; }),
+      d3.max(data, function(d) { return d.x; })
+    ])
     .range([0, width])
   let yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return d.count; })])
+    .domain([0, d3.max(data, function(d) { return d.y; })])
     .range([height, 0])
   let g = svg.append("g").attr("transform", "translate(" + 50 + "," + 50 + ")");
   g.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(xScale).tickFormat(function(d){
       return d;
-    }).ticks(5));
+    }).ticks(10));
   g.append("g")
     .call(d3.axisLeft(yScale).tickFormat(function(d){
       return d;
@@ -47,14 +35,26 @@ const createBarChart =(svg, data) =>{
     .attr("dy", "0.71em")
     .attr("text-anchor", "end")
     .text("value");
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return xScale(d.x); })
+    .attr("y", function(d) { return yScale(d.y); })
+    .attr("width", function(d) { return xScale(d.width); })
+    .attr("height", function(d) { return height - yScale(d.y); });
 }
 
 const BarPlot =(props) => {
   const ref = useRef();
   let {expressionCounts, expressionAnnotations} = props;
+  let data = [];
+  expressionCounts.map(s => {
+    data.push({x: s.range[0].start, y: s.count, width: s.range[0].end})
+  });
   useEffect(() => {
     const svg = d3.select(ref.current)
-    createBarChart(svg, expressionCounts);
+    createBarChart(svg, data);
   }, [])
 
   return(
